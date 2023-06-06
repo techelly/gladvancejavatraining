793575f84bfc8efa60724611de007341132e133a
prabhat1510 <prabhatc@gmail.com>
2023-06-06 17:14:57 +0530

    daytwo classwork

---
+++ java8featuresdaytwo/src/java8featuresdaytwo/exercise/MainApp.java
@@ -0,0 +1,20 @@
+package java8featuresdaytwo.exercise;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Customer cust1 = new Customer(1, "Stefan Walker", 1);
+		Customer cust2 = new Customer(2, "Daija Von", 1);
+		Customer cust3 = new Customer(3, "Ariane Rodriguez", 1);
+		Customer cust4 = new Customer(4, "Marques Nikolaus", 2);
+		Customer cust5 = new Customer(5, "Rachelle Greenfelder", 0);
+		Customer cust6 = new Customer(6, "Larissa White", 2);
+		Customer cust7 = new Customer(7, "Fae Heidenreich", 1);
+		Customer cust8 = new Customer(8, "Dino Will", 2);
+		Customer cust9 = new Customer(9, "Eloy Stroman", 1);
+		Customer cust10 = new Customer(10, "Brisa O Connell", 1);
+
+
+	}
+
+}
