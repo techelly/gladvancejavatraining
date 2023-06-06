f491e764ef76a0db13f77bac0a50dfb22ca4d84c
prabhat1510 <prabhatc@gmail.com>
2023-06-06 17:26:58 +0530

    daytwo classwork

--- java8featuresdaytwo/src/java8featuresdaytwo/exercise/MainApp.java
+++ java8featuresdaytwo/src/java8featuresdaytwo/exercise/MainApp.java
@@ -1,5 +1,8 @@
 package java8featuresdaytwo.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainApp {
 
 	public static void main(String[] args) {
@@ -14,6 +17,60 @@ public class MainApp {
 		Customer cust9 = new Customer(9, "Eloy Stroman", 1);
 		Customer cust10 = new Customer(10, "Brisa O Connell", 1);
 
+		List<Customer> customers = new ArrayList<Customer>();
+		customers.add(cust1);
+		customers.add(cust2);
+		customers.add(cust3);
+		customers.add(cust4);
+		customers.add(cust5);
+		customers.add(cust6);
+		customers.add(cust7);
+		customers.add(cust8);
+		customers.add(cust9);
+		customers.add(cust10);
+		
+		Product prod1 = new Product(1, "omnis quod consequatur", "Games", 184.83);
+		Product prod2 = new Product(2, "vel libero suscipit", "Toys", 12.66);
+		Product prod3 = new Product(3, "non nemo iure", "Grocery", 498.02);
+		Product prod4 = new Product(4, "voluptatem voluptas aspernatur", "Toys", 536.80);
+		Product prod5 = new Product(5, "animi cum rem", "Games", 458.20);
+		Product prod6 = new Product(6, "dolorem porro debitis", "Toys", 146.52);
+		Product prod7 = new Product(7, "aspernatur rerum qui", "Books", 656.42);
+		Product prod8 = new Product(8, "deleniti earum et", "Baby", 41.46);
+		Product prod9 = new Product(9, "voluptas ut quidem", "Books", 697.57);
+		Product prod10 = new Product(10, "eos sed debitis", "Baby", 366.90);
+		Product prod11 = new Product(11, "laudantium sit nihil", "Toys", 95.50);
+		Product prod12 = new Product(12, "ut perferendis corporis", "Grocery", 302.19);
+		Product prod13 = new Product(13, "sint voluptatem ut", "Toys", 295.37);
+		Product prod14 = new Product(14, "quos sunt ipsam", "Grocery", 534.64);
+		Product prod15 = new Product(15, "qui illo error", "Baby", 623.58);
+		Product prod16 = new Product(16, "aut ex ducimus", "Books", 551.39);
+		Product prod17 = new Product(17, "accusamus repellendus minus", "Books", 240.58);
+		Product prod18 = new Product(18, "aut accusamus quia", "Baby", 881.38);
+		Product prod19 = new Product(19, "doloremque incidunt sed", "Games", 988.49);
+		Product prod20 = new Product(20, "libero omnis velit", "Baby", 177.61);
+		Product prod21 = new Product(21, "consectetur cupiditate sunt", "Toys", 95.46);
+		Product prod22 = new Product(22, "itaque ea qui", "Baby", 677.78);
+		Product prod23 = new Product(23, "non et nulla", "Grocery", 70.49);
+		Product prod24 = new Product(24, "veniam consequatur et", "Books", 893.44);
+		Product prod25 = new Product(25, "magnam adipisci voluptate", "Grocery", 366.13);
+		Product prod26 = new Product(26, "reiciendis consequuntur placeat", "Toys", 359.27);
+		Product prod27 = new Product(27, "dolores ipsum sit", "Toys", 786.99);
+		Product prod28 = new Product(28, "ut hic tempore", "Toys", 316.09);
+		Product prod29 = new Product(29, "quas quis deserunt", "Toys", 772.78);
+		Product prod30 = new Product(30, "excepturi nesciunt accusantium", "Toys", 911.46);
+		
+		List<Product> products = new ArrayList<Product>();
+		products.add(prod1);
+		products.add(prod2);
+		products.add(prod3);
+		products.add(prod4);
+		products.add(prod5);
+		products.add(prod6);
+		products.add(prod7);
+		products.add(prod8);
+		products.add(prod9);
+		products.add(prod10);
 
 	}
 
